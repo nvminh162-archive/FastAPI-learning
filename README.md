627e8a0f363704bd1b16668c41c1380406d2184e
nvminh162 <nvminh162@gmail.com>
2026-07-28 13:47:20 +0700

    Add Alembic configuration and migration scripts for database management

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ venv\Scripts\activate
 
 python -m pip install "fastapi[standard]" sqlalchemy alembic python-multipart
 
+alembic init migrations
+
 uvicorn app.main:app --reload
--- app/db/base.py
+++ app/db/base.py
@@ -2,4 +2,10 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+# import models for Alembic can scan metadata
+from app.models.author import Author
+from app.models.book import Book
+from app.models.category import Category
+
+
 
